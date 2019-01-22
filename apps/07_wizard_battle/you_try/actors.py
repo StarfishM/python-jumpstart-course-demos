495bd4d57688b92c192ceca4ebd1851a727f9af6
merle <merle_canada@hotmail.com>
2019-01-22 15:44:37 +0100

    completed app 7 and finished the game, made creatures a base class and created creature subclasses to give different creatures different capabilities

--- apps/07_wizard_battle/you_try/actors.py
+++ apps/07_wizard_battle/you_try/actors.py
@@ -1,15 +1,6 @@
 import random
 
 
-class Wizard:
-    def __init__(self, name, level):
-        self.name = name
-        self.level = level
-
-    def attack(self, other_creature):
-        pass
-
-
 class Creature:
     def __init__(self, name, level):
         self.name = name
@@ -19,5 +10,58 @@ class Creature:
         return "Creature: {} of level {}".format(
             self.name, self.level
         )
+
+    def get_defensive_roll(self):
+        return random.randint(1, 12) * self.level
+
+
+class Dragon(Creature):
+    def __init__(self, name, level, scaliness, breaths_fire):
+        super().__init__(name, level)
+        self.scaliness = scaliness
+        self.breaths_fire = breaths_fire
+
+    def get_defensive_roll(self):
+        base_roll = super().get_defensive_roll()
+        fire_modifier = 5 if self.breaths_fire else 1
+        scale_modifier = self.scaliness/10
+
+        return base_roll*fire_modifier*scale_modifier
+
+    def breath_fire(self):
+        pass
+
+
+class SmallAnimal(Creature):
+    def get_defensive_roll(self):
+        base_roll = super().get_defensive_roll()
+        return base_roll / 2
+
+    def can_hide(self):
+        pass
+
+
+class Wizard(Creature):
+    def smile(self, creature):
+        print("The wizard {} smiles at {}".format(self.name, creature.name))
+
+        my_roll = self.get_defensive_roll()
+        #creature_roll = random.randint(1, 12) * creature.level
+        creature_roll = creature.get_defensive_roll()
+
+        print("Your wizard rolled {}...".format(my_roll))
+        print("The {} rolls {}...".format(creature.name, creature_roll))
+
+        if my_roll >= creature_roll:
+            print("The Wizard wins! And triumphs over {}".format(creature.name))
+            print()
+            return True
+        else:
+            print("The Creature {} wins! And the Wizard has been DEFEATED".format(creature.name))
+            print()
+            return False
+
+
+
 
 
--- apps/07_wizard_battle/you_try/program.py
+++ apps/07_wizard_battle/you_try/program.py
@@ -1,5 +1,7 @@
-from actors import Wizard, Creature
+from actors import Creature, Wizard, Dragon, SmallAnimal
 import random
+import time
+
 
 def main():
     print_the_header()
@@ -15,32 +17,56 @@ def print_the_header():
 
 def game_loop():
     creatures = [
-        Creature('Toad', 1),
+        SmallAnimal('Toad', 1),
         Creature('Tiger', 12),
-        Creature('Bat', 3),
-        Creature('Dragon', 50),
-        Creature('Evil Wizard', 1000)
+        SmallAnimal('Bat', 3),
+        Dragon('Dragon', 50, 75, True),
+        Wizard('Evil Wizard', 75)
 
     ]
-    print(creatures)
+    #print(creatures)
 
     hero = Wizard('Gandolf', 75)
 
 
     while True:
         active_creature = random.choice(creatures)
-        print("A {} has appeared from the dark and misty forest grounds")
+        print("A {} of level {} has appeared from the dark and misty forest grounds..."
+               .format(active_creature.name, active_creature.level))
+        print()
 
         cmd = input('Do you [s]mile, [r]un away or [l]ook around?')
         if cmd == 's':
-            print('smile')
+            if hero.smile(active_creature):
+                creatures.remove(active_creature)
+            else:
+                print("Your wizard has been defeated and needs to regain strength")
+                time.sleep(5)
+                print("The wizard returns to pick up another battle of smiles")
+                print()
+
         elif cmd == 'r':
-            print('run away')
+            print('The wizard unsure of his strength runs and hides')
+            print()
         elif cmd == 'l':
-            print('look around')
+            print('The wizard {} takes a look around the forest and sees:'.format(hero.name))
+            for c in creatures:
+                print(" * A {} of level {}".format(c.name, c.level))
+            print()
+
         else:
             print("Sorry but you did not understand my complex rules, that command does not exist... exiting game now, bye!")
             break
+
+        if not creatures:
+            print("Congratulations, you have defeated all creatures you have saved the holy grapes")
+            print("----  / ----")
+            print("--- ()() ---")
+            print("-- ()()() --")
+            print("--- ()() ---")
+            print("---- () ----")
+            break
+
 
 
 if __name__ == '__main__':
